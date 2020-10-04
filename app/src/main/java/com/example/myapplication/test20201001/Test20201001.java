5d42f7f12caab2a5aad788cb9174b060a5104a5a
張君如 <a403176845643457@gmail.com>
2020-10-04 13:51:18 +0800

    繼承implement教學

--- app/src/main/java/com/example/myapplication/test20201001/Test20201001.java
+++ app/src/main/java/com/example/myapplication/test20201001/Test20201001.java
@@ -1,6 +1,6 @@
 package com.example.myapplication.test20201001;
 
-public class Test20201001 extends Test20201001_02{
+public class Test20201001 extends Test20201001_02 {
     public static void main(String[] args){
 //        testFuntion("junacess");
         connect("junacess");
---
+++ app/src/main/java/com/example/myapplication/test20201004/Test20201004.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.test20201004;
+
+public class Test20201004 {
+}
