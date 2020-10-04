7468b3c1586b87ac0a738ce1109c79935e525a1e
張君如 <a403176845643457@gmail.com>
2020-10-04 21:09:12 +0800

    android 版面設計教學

---
+++ app/src/main/java/com/example/myapplication/test20201004/main/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.test20201004.main;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
